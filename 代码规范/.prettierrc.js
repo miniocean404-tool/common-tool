e815e5eda535c28187f1ed7972554130ecc4e4dc
MiniOcean404 <1037306928@qq.com>
2023-04-27 00:55:48 +0800

    feat: prettier

--- 代码规范/.prettierrc.js
+++ 代码规范/.prettierrc.js
@@ -7,7 +7,7 @@ module.exports = {
 	bracketSpacing: true, //对象中的空格 默认true
 	endOfLine: 'auto', //结尾是 \n \r \n\r auto
 	htmlWhitespaceSensitivity: 'ignore',
-	TrailingCooma: 'none', // 行尾逗号,默认none,可选 none|es5|all es5 包括es5中的数组、对象 all 包括函数对象等所有可选
+	trailingComma: 'none', // 行尾逗号,默认none,可选 none|es5|all es5 包括es5中的数组、对象 all 包括函数对象等所有可选
 	arrowParens: 'always', // 箭头函数参数括号 默认avoid 可选 avoid| always avoid 能省略括号的时候就省略 例如x => x always 总是有括号
 	jsxSingleQuote: false, // 在jsx中使用单引号代替双引号,
 	eslintIntegration: false, //不让prettier使用eslint的代码格式进行校验
