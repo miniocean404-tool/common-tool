3573e4de084774f76e1aafd017d8d7fd5a4d51c1
MiniOcean404 <1037306928@qq.com>
2023-07-30 01:29:12 +0800

    feat: server 日志

--- 工具/winston-logger/format.js
+++ 工具/winston-logger/format.js
@@ -2,15 +2,16 @@ const { format } = require("winston");
 
 const WINSTON_LOG_DIR = "log";
 
-const template = (info) =>
-  `[${info.level}] (${process.env.NODE_ENV}) ${[info["timestamp"]]} 信息: ${
-    info.message
-  }`;
+const template = (info) => {
+  return `[${info.level}] (${info.label}) ${[
+    info["timestamp"],
+  ]} 信息: ${JSON.stringify(info.message)}`;
+};
 
 const globalFLog = format.combine(
-  format.json(),
-  format.label({ label: `运行环境:${[process.env.NODE_ENV]}` }),
   format.timestamp({ format: "YYYY-MM-DD HH:mm:ss:SSS" }),
+  format.json(),
+  format.label({ label: `${[process.env.NODE_ENV]}` }),
   format.printf(template),
   // format.align(),
   // format.prettyPrint(),
@@ -19,6 +20,8 @@ const globalFLog = format.combine(
 const consoleReF = format.combine(
   format.colorize(), // 开启控制台颜色展示
   format.timestamp({ format: "MM-DD HH:mm:ss" }),
+  format.json(),
+  format.label({ label: `${[process.env.NODE_ENV]}` }),
   format.printf(template),
 );
 
