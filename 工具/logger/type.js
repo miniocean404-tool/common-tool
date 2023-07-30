7a3b8329b94782effbf66517ac9c3773f2177b6b
MiniOcean404 <1037306928@qq.com>
2023-07-30 15:53:01 +0800

    feat: 修改 logger

--- 工具/logger/type.js
+++ 工具/logger/type.js
@@ -1,40 +1,40 @@
-const { transports, format } = require("winston");
-const { WINSTON_LOG_DIR, consoleReF } = require("./format");
+const { transports, format } = require('winston')
+const { WINSTON_LOG_DIR, consoleReF } = require('./format')
 
-const { Console, DailyRotateFile, File } = transports;
+const { Console, DailyRotateFile, File } = transports
 
 const consoleT = new Console({
-  level: "info",
+  level: 'info',
   format: consoleReF,
-});
+})
 
 const infoFileT = new DailyRotateFile({
   // handleExceptions:true //处理异常
   // handleRejections:true //处理未经批准的拒绝承诺
   filename: `${WINSTON_LOG_DIR}/info/%DATE%.log`,
-  level: "info",
-  datePattern: "YYYY-MM-DD",
+  level: 'info',
+  datePattern: 'YYYY-MM-DD',
   zippedArchive: true,
-  maxSize: "20m",
-  maxFiles: "14d",
-});
+  maxSize: '20m',
+  maxFiles: '14d',
+})
 
 const errorFileT = new DailyRotateFile({
   filename: `${WINSTON_LOG_DIR}/err/%DATE%.log`,
-  level: "error",
-  datePattern: "YYYY-MM-DD",
+  level: 'error',
+  datePattern: 'YYYY-MM-DD',
   zippedArchive: true,
-  maxSize: "20m",
-  maxFiles: "14d",
-});
+  maxSize: '20m',
+  maxFiles: '14d',
+})
 
 const exceptionFileT = new File({
   filename: `${WINSTON_LOG_DIR}/other/exceptions.log`,
-});
+})
 
 const rejectFileT = new File({
   filename: `${WINSTON_LOG_DIR}/other/reject.log`,
-});
+})
 
 module.exports = {
   consoleT,
@@ -42,4 +42,4 @@ module.exports = {
   errorFileT,
   exceptionFileT,
   rejectFileT,
-};
+}
