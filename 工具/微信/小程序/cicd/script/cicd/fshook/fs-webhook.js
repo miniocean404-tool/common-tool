a2489b1f5aacd41445da39f46852e3b414bf9948
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2023-10-24 11:25:36 +0800

    feat: cicd

--- 工具/微信/小程序/cicd/script/cicd/fshook/fs-webhook.js
+++ 工具/微信/小程序/cicd/script/cicd/fshook/fs-webhook.js
@@ -1,45 +1,45 @@
-const ora = require('ora')
-const axios = require('axios')
-const { fsMessage } = require('./template')
+const ora = require("ora");
+const axios = require("axios");
+const { fsMessage } = require("./template");
 
 class FsWebhook {
-	postBody
-	options
+  postBody;
+  options;
 
-	constructor(options = { FsWebhookUrl: '' }) {
-		this.options = options
+  constructor(options = { FsWebhookUrl: "" }) {
+    this.options = options;
 
-		this.buildTemplate()
-	}
+    this.buildTemplate();
+  }
 
-	async send() {
-		const { FsWebhookUrl } = this.options
+  async send() {
+    const { FsWebhookUrl } = this.options;
 
-		const spinner = ora({
-			text: `正在推送飞书消息...\n`,
-			spinner: 'moon',
-		}).start()
+    const spinner = ora({
+      text: `正在推送飞书消息...\n`,
+      spinner: "moon",
+    }).start();
 
-		try {
-			const res = await axios.post(FsWebhookUrl, this.postBody)
+    try {
+      const res = await axios.post(FsWebhookUrl, this.postBody);
 
-			spinner.succeed(`推送飞书消息成功\n`)
-		} catch (error) {
-			console.error('推送飞书消息error', error)
-		}
-	}
+      spinner.succeed(`推送飞书消息成功\n`);
+    } catch (error) {
+      console.error("推送飞书消息error", error);
+    }
+  }
 
-	buildTemplate() {
-		const { branchName, hash, message, wechatPart, machine } = this.options
+  buildTemplate() {
+    const { branchName, hash, message, wechatPart, machine } = this.options;
 
-		this.postBody = fsMessage({
-			wechatPart,
-			machine,
-			branchName: branchName,
-			hash: hash,
-			msg: message,
-		})
-	}
+    this.postBody = fsMessage({
+      wechatPart,
+      machine,
+      branchName,
+      hash,
+      message,
+    });
+  }
 }
 
-module.exports = FsWebhook
+module.exports = FsWebhook;
--- 工具/微信/小程序/cicd/script/cicd/wx-ci.js
+++ 工具/微信/小程序/cicd/script/cicd/wx-ci.js
@@ -1,40 +1,39 @@
 // wx-ci.js
-const path = require('path')
-const fs = require('fs')
-const ci = require('miniprogram-ci')
-const { execSync } = require('child_process')
-const { fail, info, success } = require('./util')
+const path = require("path");
+const fs = require("fs");
+const ci = require("miniprogram-ci");
+const { execSync } = require("child_process");
+const { fail, info, success } = require("./util");
 
 class WxCi {
-	constructor() {}
+  constructor() {}
 
-	createProject(appid, type, projectPath, privateKeyPath) {
-		// 校验密钥
-		if (!fs.existsSync(privateKeyPath)) {
-			fail(`${privateKeyPath} 密钥文件不存在`)
-			process.exit(1)
-		}
+  createProject(appid, type, projectPath, privateKeyPath) {
+    // 校验密钥
+    if (!fs.existsSync(privateKeyPath)) {
+      fail(`${privateKeyPath} 密钥文件不存在`);
+      process.exit(1);
+    }
 
-		this.project = new ci.Project({ appid, type, projectPath, privateKeyPath })
-	}
+    this.project = new ci.Project({ appid, type, projectPath, privateKeyPath });
+  }
 
-	async upload(version, robot) {
-		info('正在上传...')
+  async upload({ version, robot, setting, desc }) {
+    info("正在上传...");
 
-		try {
-			info('上传体验版...')
+    try {
+      info("上传体验版...");
 
-			const desc = execSync('git log --pretty=format:%s HEAD -1', { cwd: process.cwd() }).toString().trim()
-			await ci.upload({ project: this.project, version, desc, robot })
+      await ci.upload({ project: this.project, version, desc, robot, setting });
 
-			success('上传成功')
-		} catch (error) {
-			fail(`上传失败: ${error}`)
-			process.exit(1)
-		}
-	}
+      success("上传成功");
+    } catch (error) {
+      fail(`上传失败: ${error}`);
+      process.exit(1);
+    }
+  }
 
-	createQRCode() {}
+  createQRCode() {}
 }
 
-module.exports = WxCi
+module.exports = WxCi;
