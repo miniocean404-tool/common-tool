6fa9bef9996e20b41f4c65f9a4cb583daa836b4b
MiniOcean404 <1037306928@qq.com>
2023-12-04 23:11:13 +0800

    feat: 截图

---
+++ 工具/截图/index.js
@@ -0,0 +1,40 @@
+import domtoimage from "dom-to-image";
+
+const screenshot = async (dom) => {
+  window.devicePixelRatio;
+  const base64 = await domtoimage.toSvg(dom);
+  return base64;
+};
+
+// 原理是将画布放大 N 倍后，将图片缩小 N 倍，这样就可以得到高清的图片了
+function imageToPngBase64(image, scale) {
+  const canvas = document.createElement("canvas");
+  const ctx = canvas.getContext("2d");
+
+  scale = window.devicePixelRatio * scale;
+
+  canvas.width = image.width * scale;
+  canvas.height = image.height * scale;
+
+  ctx?.scale(scale, scale);
+  ctx && ctx.drawImage(image, 0, 0, image.width, image.height);
+  return canvas.toDataURL("image/png");
+}
+
+async function download(filename, url) {
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = filename;
+  a.click();
+}
+
+const exec = async () => {
+  const SVGBase64 = await screenshot();
+  const image = new Image();
+  image.src = SVGBase64;
+
+  image.onload = () => {
+    const PNGBase64 = imageToPngBase64(image, 2);
+    download("image.png", PNGBase64);
+  };
+};
