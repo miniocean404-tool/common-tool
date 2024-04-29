818aa7259d9d9ee4c957bb1b60ca97841ca57e6e
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-04-29 10:35:44 +0800

    feature: this

---
+++ 平台/Vue/setup 获取 this.vue
@@ -0,0 +1,10 @@
+<template>
+  <div></div>
+</template>
+
+<script setup lang="ts">
+import { getCurrentInstance } from "vue";
+const $this = getCurrentInstance()?.proxy
+</script>
+
+<style scoped></style>
