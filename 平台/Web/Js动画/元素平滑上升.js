8bddd582f2d6e6615f9f0c5eae0d1f509d424e3b
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-05-15 15:39:23 +0800

    feature: 元素平滑上升

--- 平台/Web/Js动画/元素平滑上升.js
+++ 平台/Web/Js动画/元素平滑上升.js
@@ -1,34 +1,12 @@
 const map = new Map();
 
-let ob;
-const getIntersectionObserver = () => {
-  if (ob) return ob;
-
-  ob = new IntersectionObserver(
-    (entries) => {
-      for (const entry of entries) {
-        const { target, isIntersecting } = entry;
-
-        if (isIntersecting) {
-          const animation = map.get(target);
-          animation.play();
-
-          ob.unobserve(target);
-        }
-      }
-    },
-    { threshold: 0.0 },
-  );
-  return ob;
-};
-
 export function elementRiseSmoothly(options) {
   options = { ...{ distant: 100, duration: 500 }, ...options };
 
   const { el, distant, duration } = options;
   if (!isExistViewport(el)) return;
 
-  getIntersectionObserver();
+  const ob = getInstanceWarrper(getIntersectionObserver);
 
   const animation = el.animate(
     [
@@ -43,7 +21,43 @@ export function elementRiseSmoothly(options) {
   map.set(el, animation);
 }
 
+function getIntersectionObserver() {
+  const ob = new IntersectionObserver(
+    (entries) => {
+      for (const entry of entries) {
+        const { target, isIntersecting } = entry;
+
+        if (isIntersecting) {
+          const animation = map.get(target);
+          animation.play();
+
+          ob.unobserve(target);
+        }
+      }
+    },
+    { threshold: 0.0 },
+  );
+
+  return ob;
+}
+
 function isExistViewport(el) {
   const rect = el.getBoundingClientRect();
   return rect.top > window.innerHeight;
+}
+
+function getInstanceWarrper(fn) {
+  const getInstance = (function () {
+    var instance;
+
+    return (fn) => {
+      if (!instance) {
+        if (typeof fn === "function") instance = fn();
+        if (typeof fn === "object") instance = new fn();
+      }
+      return instance;
+    };
+  })();
+
+  return getInstance(fn);
 }
