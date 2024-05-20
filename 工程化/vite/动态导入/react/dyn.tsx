0e62ff306c9d8fd9aab68ed2f89d9b7e176ef502
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-05-20 17:27:05 +0800

    feature: 动态路由

---
+++ 工程化/vite/动态导入/react/dyn.tsx
@@ -0,0 +1,36 @@
+import { lazy } from "react"
+
+type MetaConfig = Record<string | number | symbol, unknown>
+
+export const dynRouters: Array<any> = getRoutes()
+
+function getRoutes() {
+  const pages = import.meta.glob("/src/(pages|view)/**/index.[jt]sx")
+
+  const configs = import.meta.glob<MetaConfig>([`/src/(pages|view)/**/config.[tj]s`], {
+    // 获取导出的模块
+    eager: true,
+    // 直接获取 default 结果
+    import: "default",
+  })
+
+  return Object.entries(pages).map(([path, element]) => {
+    const meta = configs[path.replace("index.tsx", "config.ts")]
+    path = path.replace(/\/src\/[pages|views]+(?<path>.*)\/index.[tj]sx/gims, "$<path>") || "/"
+
+    const name = path.split("/").join("-")
+
+    // 需要使用下方方式加载
+    // <Suspense fallback={<div></div>}>
+    //   <RouterProvider router={router} />
+    // </Suspense>
+    const Page = lazy(element as any)
+
+    return {
+      path,
+      name,
+      meta,
+      element: <Page />,
+    }
+  })
+}
