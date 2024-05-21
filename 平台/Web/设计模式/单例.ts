7d297f1bfa4ddec2526ba4e5f9aae820393573d1
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-05-21 16:26:31 +0800

    feature: 单例

--- 平台/Web/设计模式/单例.ts
+++ 平台/Web/设计模式/单例.ts
@@ -1,11 +1,15 @@
-export const getInstance = (function () {
+// 1.
+const getInstanceWrapper = (fn) => {
   let instance
 
-  return (fn: any) => {
-    if (!instance) {
-      if (typeof fn === "function") instance = fn()
-      if (typeof fn === "object") instance = new fn()
-    }
+  return function (...res) {
+    if (!instance) instance = fn.apply(this, res)
     return instance
   }
-})()
+}
+
+// 传入任意函数得到一个可以获取这个函数 单例对象 的函数，使用 instance(params)
+const instance = getInstanceWrapper(() => {})
+
+// 2. ESM 默认导出其他地方导入也是单例
+export default {}
