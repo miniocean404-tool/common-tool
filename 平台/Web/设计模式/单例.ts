67c0dbd441bce34bda7b84f74efcbf26e67bfde1
MiniOcean404 <1037306928@qq.com>
2024-06-04 21:38:29 +0800

    📝 memo: 添加单例模式类型声明

--- 平台/Web/设计模式/单例.ts
+++ 平台/Web/设计模式/单例.ts
@@ -1,8 +1,8 @@
 // 1.
-const getInstanceWrapper = (fn) => {
-  let instance
+export const getInstanceWrapper = <T extends (...args: any) => any>(fn: Function) => {
+  let instance: ReturnType<T>
 
-  return function (...res) {
+  return function (this: any, ...res: Parameters<T>): ReturnType<T> {
     if (!instance) instance = fn.apply(this, res)
     return instance
   }
