d6953c1235cca179525793dfbb8ad467d5c10fe7
MiniOcean404 <1037306928@qq.com>
2024-06-25 00:36:01 +0800

    📝 memo: 跑马灯边框

--- 平台/Web/js/获取当前尺寸的设备类型/index.js
+++ 平台/Web/js/获取当前尺寸的设备类型/index.js
@@ -3,5 +3,5 @@ export const getSizeDeviceType = () => {
     isMobile: matchMedia("screen and (max-width: 767px)").matches,
     isTablet: matchMedia("screen and  (max-width: 1023px) and (min-width: 768px)").matches,
     isDesktop: matchMedia("screen and (min-width: 1023.1px)").matches,
-  };
-};
+  }
+}
