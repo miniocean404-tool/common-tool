ab20f0075cb55ea78d689a161bca5c0375f47341
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-06-28 15:23:37 +0800

    📝 memo: 隐私协议

--- 平台/微信/小程序/taro/组件/隐私协议/privacy.js
+++ 平台/微信/小程序/taro/组件/隐私协议/privacy.js
@@ -12,8 +12,23 @@ export const renderPrivacy = (resolve) => {
     <Privacy
       onCancel={onClose}
       onOk={resolve}
-      title={`达芬骑 ${miniProgramName} 隐私保护指引`}
-      desc={`在使用「${miniProgramName}」小程序服务前，请仔细阅读%%webview:《达芬骑 ${miniProgramName} 隐私保护指引》:openPrivacyContract%%。如你同意《${miniProgramName} 隐私保护指引》，请点击【同意】开始使用「${miniProgramName}」小程序。`}
+      title={`[品牌名] ${miniProgramName} 隐私保护指引`}
+      desc={`在使用「${miniProgramName}」小程序服务前，请仔细阅读%%webview:《[品牌名] ${miniProgramName} 隐私保护指引》:openPrivacyContract%%。如你同意《${miniProgramName} 隐私保护指引》，请点击【同意】开始使用「${miniProgramName}」小程序。`}
     />
   ))
 }
+
+// 在 hook 中使用
+export const onReady = [
+  () => {
+    // 查看是否授权过
+    // getPrivacySetting({
+    //   success: (res) => console.log(res),
+    //   fail: () => {},
+    // })
+    // onNeedPrivacyAuthorization((resolve, e) => {
+    //   console.log('触发本次事件的接口是：' + e.referrer)
+    //   renderPrivacy(resolve)
+    // })
+  },
+]
