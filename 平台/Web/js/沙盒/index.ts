04ef95754e2dea9e7502ed4e287e62681cb46913
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-07-18 13:49:09 +0800

    📝 memo: 沙盒

---
+++ 平台/Web/js/沙盒/index.ts
@@ -0,0 +1,48 @@
+// NodeJs 需要添加
+const window = global
+
+function createSandboxCtxProxy() {
+  return new Proxy(
+    {},
+    {
+      get(target, key, _receiver) {
+        // 优先从代理对象上取值
+        if (Reflect.has(target, key)) {
+          return Reflect.get(target, key)
+        }
+
+        // 如果找不到，就直接从 window 对象上取值
+        const rawValue = Reflect.get(window, key)
+
+        // 如果兜底的是一个函数，需要绑定 window 对象，比如 window.addEventListener
+        if (typeof rawValue === "function") {
+          const valueStr = rawValue.toString()
+          if (!/^function\s+[A-Z]/.test(valueStr) && !/^class\s+/.test(valueStr)) {
+            return rawValue.bind(window) // 所有 window 上非构造函数调用时候的 this 绑定 window 对象
+          }
+        }
+
+        // 其他情况直接返回
+        return rawValue
+      },
+      set(target, key, value, _receiver) {
+        return Reflect.set(target, key, value)
+      },
+    },
+  )
+}
+
+function sandbox(code: string) {
+  const boxCtx = createSandboxCtxProxy()
+
+  // with 语句的能力可以这样理解：在 js 运行时将指定代码段的执行上下文设置为指定的对象，从而调整该代码端的作用域链。 使用 with，我们可以这样改动并且测试沙箱代码：
+  const boxFn = `
+  (function boxFn(window) {
+    with(window){ ${code} }
+  })(boxCtx)`
+  // 通过 eval 函数执行子应用代码
+  eval(boxFn)
+}
+
+const execCode = '(function () { window.a = 2; window.b = "b"; window.console.log ("a,b", a, b) })()'
+sandbox(execCode)
