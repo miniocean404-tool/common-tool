81e941dca50f22c4b98b4766fdd277dc4e75def0
MiniOcean404 <1037306928@qq.com>
2024-07-30 23:17:55 +0800

    📝 memo: 注释

--- 平台/Typescript/防抖类型标注.ts
+++ 平台/Typescript/防抖类型标注.ts
@@ -2,7 +2,13 @@ function sum(a: number, b: number): number {
   return a + b
 }
 
-declare function debounce<T extends any[]>(fn: (...args: T) => any, delay: number): (...args: T) => void
+declare function debounce<T extends any>(fn: (...args: T[]) => any, delay: number): (...args: T[]) => void
 
 const dSum = debounce(sum, 1000)
-const a = dSum(1, 2)
+
+// 有 ...args: T[] 可以直接传递扩展
+const a = dSum(...[1, 2])
+
+// 没有 ...args: T[] 的情况，传递扩展
+const params = [1, 2] as const
+Math.atan2(...params)
