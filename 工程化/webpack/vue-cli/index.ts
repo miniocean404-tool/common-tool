38167f06858f0e138cd4434f2e77d5ae1e2e0ccb
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-08-08 16:58:31 +0800

    📝 memo: 注释

--- 工程化/webpack/vue-cli/index.ts
+++ 工程化/webpack/vue-cli/index.ts
@@ -1,5 +1,6 @@
 import path from "path"
 
+// https://github.com/nuxt/nuxt/issues/9223
 // nuxt2 只需要添加 build 下的 transpile,在 transpile 中添加需要处理的 esModule 的包即可
 // vue 添加 babel-loader 处理 es 文件
 function add_babel(config: any) {
