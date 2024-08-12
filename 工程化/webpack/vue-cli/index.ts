6a58f35a39038db333182f4b8732a3dd90488509
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-08-12 14:15:10 +0800

    📝 memo: vue-cli

--- 工程化/webpack/vue-cli/index.ts
+++ 工程化/webpack/vue-cli/index.ts
@@ -4,7 +4,7 @@ import path from "path"
 // nuxt2 只需要添加 build 下的 transpile,在 transpile 中添加需要处理的 esModule 的包即可
 // vue 添加 babel-loader 处理 es 文件
 function add_babel(config: any) {
-  const esLIbs = ["@svgdotjs"]
+  const esLIbs = [({ isClient }) => isClient && "@svgdotjs/svg.js","change-case"]
 
   config.module.rules.push({
     test: /\.m?jsx?$/i,
