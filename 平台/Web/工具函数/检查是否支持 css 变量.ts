4a47c330055499ef507df7944dc2826271b35717
MiniOcean404 <1037306928@qq.com>
2024-08-16 22:27:33 +0800

    📝 memo: 是否支持

---
+++ 平台/Web/工具函数/检查是否支持 css 变量.ts
@@ -0,0 +1,9 @@
+export const supportsCssVars = function () {
+  document.documentElement.className = "js"
+  let temp = document.createElement("style")
+  temp.innerHTML = "root: { --tmp-var: bold; }"
+  document.head.appendChild(temp)
+  const isSupport = !!(window.CSS && window.CSS.supports && window.CSS.supports("font-weight", "var(--tmp-var)"))
+  temp.parentNode?.removeChild(temp)
+  return isSupport
+}
