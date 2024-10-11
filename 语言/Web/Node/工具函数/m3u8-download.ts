f3f722b65f9b82f78b01e897e61cc0185b0e6c12
MiniOcean404 <1037306928@qq.com>
2024-10-11 21:28:37 +0800

    ✨ feat: ffmpeg

---
+++ 语言/Web/Node/工具函数/m3u8-download.ts
@@ -0,0 +1,77 @@
+// 文章：https://juejin.cn/post/7382966707060703268?share_token=91713cfd-d861-496c-bdc0-db4ea0e75844#heading-0
+
+import dayjs from "dayjs"
+import ffmpeg from "fluent-ffmpeg"
+import fs from "fs"
+// @ts-ignore
+import { Parser } from "m3u8-parser"
+import path from "path"
+import z from "zod"
+
+export async function download(url: string) {
+  // 1. 解析 m3u8 文件
+  const content = await fetch(url).then((res) => res.text())
+  const parser = new Parser()
+
+  parser.push(content)
+  parser.end()
+
+  if (!isValidManifest(parser.manifest)) return { success: false, message: `Unvalid Manifest` }
+
+  const segments =
+    parser.manifest.segments?.map((segment: any, i: number) => ({
+      uri: parseUri(url, segment.uri),
+      index: i,
+    })) ?? []
+
+  const downloaded = []
+  await Promise.allSettled(
+    segments?.map(async (segment) => {
+      const fileId = `${segment.index}.ts`
+      const res = await fetch(segment.uri)
+      const file = await res.arrayBuffer()
+
+      fs.writeFileSync(path.join(process.cwd(), `./data/${fileId}`), Buffer.from(file))
+      downloaded.push(fileId)
+    }),
+  )
+  const downloadedIds = [...downloaded].sort((a, b) => parseInt(a.split(".")[0]) - parseInt(b.split(".")[0]))
+
+  // 3. 合并所有片段
+  const outputFileName = dayjs().format("YYYY-MM-DDTHH:mm:ss")
+
+  await new Promise((resolve, reject) => {
+    var videos = ffmpeg()
+
+    downloadedIds.forEach(function (videoName) {
+      videos = videos.addInput(path.join(process.cwd(), `./data/${videoName}`))
+    })
+    // fluent-ffmpeg 必须条件：https://github.com/fluent-ffmpeg/node-fluent-ffmpeg?tab=readme-ov-file#prerequisites
+    videos
+      .mergeToFile(path.join(process.cwd(), `./data/${outputFileName}.mp4`), path.join(process.cwd(), `./data`))
+      .on("error", function (err) {
+        console.log("An error occurred: " + err.message)
+      })
+      .on("end", function () {
+        console.log("Merging finished !")
+      })
+  })
+}
+
+function isValidManifest(manifest: any) {
+  const segmentsLength = manifest.segments?.length ?? 0
+  const playlistsLength = manifest.playlists?.length ?? 0
+  return segmentsLength > 0 || playlistsLength > 0
+}
+
+function parseUri(base: string, uri: string) {
+  const isUrl = z.string().url().safeParse(uri).success
+  if (isUrl) return uri
+
+  const baseURL = new URL(base)
+  const basePathname = baseURL.pathname.replace(/\/+$/, "").split("/").slice(0, -1).join("/")
+  const pathname = `${basePathname}/${uri}`
+  const parsedURL = new URL(pathname, baseURL)
+
+  return parsedURL.toString()
+}
