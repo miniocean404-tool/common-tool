6dc426b4d93ca57151f5cb5ba846239908e532e1
MiniOcean404 <1037306928@qq.com>
2024-10-14 20:58:47 +0800

    ✨ feat: 关闭端口

--- 系统/Windows/index.ts
+++
@@ -1,5 +0,0 @@
-// 打开当前路径下的文件夹
-console.log(`explorer .`)
-// start https://www.baidu.com/ 打开网页
-// start file://xxxx 打开文件
-console.log(`start .`)
--- 系统/资源管理器.ts
+++ 系统/资源管理器.ts
@@ -1,5 +1,6 @@
 const getDesk = false
 
+// Mac
 // 获取当前打开的 finder 的路径，或桌面路径
 console.log(
   `osascript -e 'tell application "Finder" to get the POSIX path of ${
@@ -11,3 +12,10 @@ console.log(
 // open https://www.baidu.com/ 打开网页
 // open file://xxxx 打开文件
 console.log(`open .`)
+
+// Windows
+// 打开当前路径下的文件夹
+console.log(`explorer .`)
+// start https://www.baidu.com/ 打开网页
+// start file://xxxx 打开文件
+console.log(`start .`)
