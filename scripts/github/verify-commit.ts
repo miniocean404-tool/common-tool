d73536aed1f91bbf0b917f5ad01a94e7d7e342ed
MiniOcean404 Davinci <haiyang.dai@davincimotor.com>
2024-10-23 14:46:59 +0800

    ✨ feat: git

--- scripts/github/verify-commit.ts
+++ scripts/github/verify-commit.ts
@@ -1,9 +1,9 @@
 // Invoked on the commit-msg git hook by yorkie.
 
-const chalk = require('chalk')
+const chalk = require("chalk")
 // Yorkie的gitHooks使用process.env.GIT_PARAMS
 const msgPath = process.argv[2]
-const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
+const msg = require("fs").readFileSync(msgPath, "utf-8").trim()
 
 // revert:回滚到上一个版本
 
@@ -19,20 +19,19 @@ const msg = require('fs').readFileSync(msgPath, 'utf-8').trim()
 // ci:持续集成修改
 // chore:其他修改, 比如改变构建流程、或者增加依赖库、工具等
 
-const commitRE =
-  /^(revert: )?(dev|feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?(.{1,10})?: .{1,50}/
+const commitRE = /^(.* )?(revert: )?(dev|feat|fix|docs|dx|style|refactor|perf|test|workflow|build|ci|chore|types|wip|release)(\(.+\))?(.{1,10})?: .{1,50}/
 const mergeRe = /^(Merge pull request|Merge branch)/
 
 if (!commitRE.test(msg)) {
   if (!mergeRe.test(msg)) {
     console.log(msg)
     console.error(
-      `  ${chalk.bgRed.white(' 错误 ')} ${chalk.red(`错误的消息格式`)}\n\n${chalk.red(
-        `  自动化校验-提交模板样例:\n\n`
+      `  ${chalk.bgRed.white(" 错误 ")} ${chalk.red(`错误的消息格式`)}\n\n${chalk.red(
+        `  自动化校验-提交模板样例:\n\n`,
       )}    ${chalk.green(`feat(compiler): add 'comments' option（冒号后有空格）`)}\n` +
         `    ${chalk.green(`fix(v-model): handle events on blur (close #28)`)}\n\n${chalk.red(
-          `  查看文档详情：https://github.com/vuejs/vue-next/blob/master/.github/commit-convention.md for more details.\n`
-        )}`
+          `  查看文档详情：https://github.com/vuejs/vue-next/blob/master/.github/commit-convention.md for more details.\n`,
+        )}`,
     )
     process.exit(1)
   }
