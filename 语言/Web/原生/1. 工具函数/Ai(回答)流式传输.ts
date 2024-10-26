3496651286acf7c823afd6b7b00090e4e5c214d2
MiniOcean404 <1037306928@qq.com>
2024-10-26 18:01:31 +0800

    ✨ feat: SSE

---
+++ 语言/Web/原生/1. 工具函数/Ai(回答)流式传输.ts
@@ -0,0 +1,20 @@
+// Ai 回答一个字符一个字符展示的写法 适用于 SSE 的流式传输，text/stream
+const init = async () => {
+  // 代表详情头刚好到达
+  const res = await fetch("https://www.imooc.com/api/http/ai?name=imooc", {
+    method: "POST",
+    body: JSON.stringify({ name: "imooc" }),
+  })
+
+  // 代表第一个字节到达，但是还没有到达详情头，使用 ReadableStream 读取
+  const reader = res.body?.getReader()
+
+  while (true) {
+    const textDecoder = new TextDecoder()
+    const { done, value } = await reader?.read()!
+    if (done) break
+
+    const txt = textDecoder.decode(value)
+    console.log(txt)
+  }
+}
